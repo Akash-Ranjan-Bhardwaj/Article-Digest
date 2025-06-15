2145820a1e15f2145c40a3c30df632e99cb9885b
Indranil Mukherjee <rpmukherjee@gmail.com>
2025-06-15 21:23:54 +0530

    Updated the article Routes

--- backend/routes/articleRoutes.js
+++ backend/routes/articleRoutes.js
@@ -1,13 +1,35 @@
 // Importing required modules
-const express = require('express');
-const { getAllArticles, getArticle, createArticle, updateArticle, deleteArticle } = require('../controllers/articleController.js');
+const express = require ('express');
+const {getAllArticles, 
+       searchArticles,
+       getArticleBySlug, 
+       getArticleComments, 
+       createArticle, 
+       updateArticle, 
+       toggleLike, 
+       toggleBookmark, 
+       deleteArticle, 
+       generateSummary} = require('../controllers/articleController.js');
+const {checkArticleOwnership, protect} = require('../middleware/authMiddleware.js');
+const { uploadArtilceImage } = require('../middleware/uploadMiddleware.js');
 
-// Initializing the router
-const articleRouter = express.Router();
 
-// Initializing the Routes
-articleRouter.get("/getAllArticles", getAllArticles)
-articleRouter.get("/getArticle", getArticle)
-articleRouter.post('/createArticle', createArticle)
-articleRouter.put('/updateArticle', updateArticle)
-articleRouter.delete('/deleteArticle', deleteArticle)
+// Initializing Article Router
+const Articlerouter = express.Router();
+
+Articlerouter.get('/', getAllArticles);
+Articlerouter.get('/search', searchArticles);
+Articlerouter.get('/:slug', getArticleBySlug);
+Articlerouter.get('/:slug/comments', getArticleComments);
+
+Articlerouter.use(protect);
+
+Articlerouter.post('/', uploadArtilceImage.single('thumbnail'), createArticle);
+Articlerouter.patch('/:slug', checkArticleOwnership, uploadArtilceImage.single('thumbnail'), updateArticle);
+Articlerouter.delete('/:slug', checkArticleOwnership, deleteArticle);
+
+Articlerouter.post('/:slug/like', toggleLike);
+Articlerouter.post('/:slug/bookmark', toggleBookmark);
+Articlerouter.post('/:slug/generate-summary', generateSummary);
+
+module.exports = Articlerouter;
